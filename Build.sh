0d8bc90fbfe262104c33eb557655da194375b39d
The Mystery <132539789+notthemystery@users.noreply.github.com>
2026-08-04 17:12:39 +0530

    Update Build.sh

--- Build.sh
+++ Build.sh
@@ -36,7 +36,7 @@ DD_APP_PATH="$WORKING_LOCATION/build/DerivedDataApp/Build/Products/Debug-iphoneo
 TARGET_APP="$WORKING_LOCATION/build/$APPLICATION_NAME.app"
 cp -r "$DD_APP_PATH" "$TARGET_APP"
 else
-xcodebuild -project "$WORKING_LOCATION/$APPLICATION_NAME.xcodeproj" \
+xcodebuild -project "/ios/Runner.xcodeproj" \
     -scheme "$APPLICATION_NAME" \
     -configuration Release \
     -derivedDataPath "$WORKING_LOCATION/build/DerivedDataApp" \
